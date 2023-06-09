29bb1f4d77f99afe963a45860c4285291f27a555
Ateeb Khan <ateebkhan669@gmail.com>
2023-06-09 19:29:10 +0530

    Files

---
+++ main.py
@@ -0,0 +1,31 @@
+# Importing the required libraries
+import cv2
+import easyocr
+import matplotlib.pyplot as plt
+
+# Reading the Image
+img_path = 'Screenshot (20).jpg'
+
+img = cv2.imread(img_path)
+
+# Text detector
+
+detector = easyocr.Reader(['en'],gpu=False)
+
+text_ = detector.readtext(img)
+
+threshold = 0.25
+
+# draw the bounding box and type text
+
+for t_, t in enumerate(text_):
+    print(t)
+
+    bbox, text, score = t
+
+    if score>threshold:
+        cv2.rectangle(img,bbox[0],bbox[2],(0,0,255),5)
+        cv2.putText(img,text,bbox[0],cv2.FONT_HERSHEY_COMPLEX,0.65,(0,255,0),2)
+
+plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+plt.show()
